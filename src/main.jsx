2e6ea55266bad1d945204f6a812fc203bece7f94
kvmprojetos-png <kvmprojetos@gmail.com>
2026-06-12 13:53:42 -0300

    Fix import path for KMZeroApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import KMZeroApp from './kmzero (2).jsx';
+import KMZeroApp from './KMZeroApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
