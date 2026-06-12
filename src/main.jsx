c32a4f12b24409a5c26ac0eef0e1efc985957b06
kvmprojetos-png <kvmprojetos@gmail.com>
2026-06-11 23:04:59 -0300

    Ativa versao Fase 2: fix sistemico de filtros + sync de RDOs (aponta main.jsx para o arquivo novo)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import KMZeroApp from './KMZeroApp.jsx';
+import KMZeroApp from './kmzero (2).jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
